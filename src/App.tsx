58cf093aaf22ce3e4d3cca01ec9fd510fd7ffd62
Joaquim <johnboxcodes@gmail.com>
2021-03-19 21:38:31 -0300

    adds react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import {Helmet} from "react-helmet";
 
 export default function App() {
   return (
-    <div>
+    <div className="application--container">
+       <Helmet>
+                <meta charSet="utf-8" />
+                <title>Address book</title>
+                <link rel="canonical" href="https://addressbook-jb.herokuapp.com" />
+       </Helmet>
       <h1>hello, world!</h1>
     </div>
   );

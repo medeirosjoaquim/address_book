852cb404a2bfff81c3ddd674bea30a32cdf804d2
Joaquim <johnboxcodes@gmail.com>
2021-03-20 16:53:46 -0300

    updates basic component calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import {Helmet} from "react-helmet";
-import Navbar from "./components/navbar/Navbar";
-import UsersList from "./components/users_list/Users_List";
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import Navbar from './components/navbar/Navbar';
+import UsersList from './components/users_list/Users_List';
 import './App.scss';
 export default function App() {
   return (
     <div className="application--container">
-       <Helmet>
-                <meta charSet="utf-8" />
-                <title>Address book</title>
-                <link rel="canonical" href="https://addressbook-jb.herokuapp.com" />
-       </Helmet>
-       <Navbar/>
-       <UsersList/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Address book</title>
+        <link rel="canonical" href="https://addressbook-jb.herokuapp.com" />
+      </Helmet>
+      <Navbar />
+      <UsersList />
     </div>
   );
 }

e3400b1dbf908f13e5990214e5c25587f46a5bf7
Joaquim <johnboxcodes@gmail.com>
2021-03-21 19:06:08 -0300

    updates number of fetched users

--- src/consts/baseUrl.ts
+++ src/consts/baseUrl.ts
@@ -1,1 +1,1 @@
-export const baseUrl = (quantity = 50) => `https://randomuser.me/api/?seed=sherpany&results=${quantity}`;
+export const baseUrl = (quantity = 150) => `https://randomuser.me/api/?seed=sherpany&results=${quantity}`;

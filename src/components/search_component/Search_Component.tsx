cc4f9da059e37ee1ef4797f2823d4c9efc43a00b
Joaquim <johnboxcodes@gmail.com>
2021-03-21 19:06:31 -0300

    handles search input with debounce

--- src/components/search_component/Search_Component.tsx
+++ src/components/search_component/Search_Component.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './search-component.scss';
 import Select from 'react-select';
 import {dropdownStyles} from './dropdown.styles';
+import { MainContext } from '../../context/app.context';
+import { debounce } from '../../helpers/debounce';
 
 const options = [
   { value: 'name', label: 'Name' },
@@ -10,15 +12,22 @@ const options = [
 ];
 
 function SearchComponent() {
+  const [appContext, setAppContext] = useContext(MainContext)
+  const setAppContextDebounce = debounce(setAppContext, 500)
+
+  const handleInputChange = (inputValue: string) => {
+    setAppContextDebounce({...appContext, searchText: inputValue})
+    console.log(setAppContext)
+  }
   return (
     <div className="search-component--container column">
       <div className="row">
-        <div className="input-field"><input placeholder="Search" id="search-input" /></div>
+        <div className="input-field"><input placeholder="Search" 
+        onChange={e => handleInputChange(e.target.value)}
+        id="search-input" /></div>
         <Select isSearchable={false}
           placeholder={'search by'.toUpperCase()}
-
           styles={{...dropdownStyles}} options={options} />
-
       </div>
     </div>
   );

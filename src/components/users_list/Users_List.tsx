68d9fec735a0d5f84cdedf61e028db63ed0fd8c2
Joaquim <johnboxcodes@gmail.com>
2021-03-21 21:01:01 -0300

    fix search

--- src/components/users_list/Users_List.tsx
+++ src/components/users_list/Users_List.tsx
@@ -1,15 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import './users-list.scss';
 import {useFetch} from '../../hooks/useFetch';
 import {baseUrl} from '../../consts/baseUrl';
 import {IUser} from '../../models/user.model';
 import UserRow from '../user-row/user-row';
 import { MainContext } from '../../context/app.context';
+import { removeDiacritics } from '../../helpers/removeDiacritics';
 
 // TODO: use react-virtualized to render list
 
 const UsersList = () => {
-  const [appContext, setAppContext] = useContext(MainContext)
+  const [appContext, _] = useContext(MainContext)
   const {status, data} = useFetch<{info: {}; results: IUser[]}>(baseUrl(), {params: {nat: ''}});
   
   const filterNationality = (users: IUser[], natArray: string[]): IUser[] => {
@@ -20,15 +21,39 @@ const UsersList = () => {
     }
   }
 
-  console.log(data)
+  const filterSearch = (users: IUser[], searchText: string, searchKey = 'name'): IUser[] => {
+    searchText = removeDiacritics(searchText.toLowerCase())
+    if (searchText.length < 1) {
+      return users
+    } else {
+      switch (searchKey) {
+        case 'name':
+          return users.filter(user => removeDiacritics(user.name.first.toLowerCase()).includes(searchText) 
+          || removeDiacritics(user.name.last.toLowerCase()).includes(searchText))
+        case 'address':
+          return users.filter(user => 
+            removeDiacritics(user.location.street.name.toLowerCase()).includes(searchText) 
+          || removeDiacritics(user.location.city.toLowerCase()).includes(searchText)
+          || removeDiacritics(user.location.state.toLowerCase()).includes(searchText)
+          || removeDiacritics(user.location.country.toLowerCase()).includes(searchText)
+          ) 
+        case 'email':
+          return users.filter(user => removeDiacritics(user.email.toLowerCase()).includes(searchText))                    
+        default:
+          break;
+      }
+      return users.filter( user => user[searchKey as keyof IUser] === searchText)
+    }
+  }
+
+
   if (status === 'fetched') {
-    
-    const users = filterNationality(data.results,  appContext.filterNationality)
+    let users = filterNationality(data.results,  appContext.filterNationality)
+    users = filterSearch(users, appContext.searchText, appContext.searchKey)
     return (
       <div className="users-list--container">
-        {/* <span>{status}</span> */}
         {users.map(user => (
-          <UserRow key={user.login.uuid} name={user.name} location={user.location} picture={user.picture} />
+          <UserRow key={user.login.uuid} name={user.name} email={user.email} location={user.location} picture={user.picture} />
         ))}
       </div>
     );

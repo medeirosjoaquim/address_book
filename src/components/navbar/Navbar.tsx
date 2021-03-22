740e97bdc71287e1cb24c7cbb621bd2ab1034248
Joaquim <johnboxcodes@gmail.com>
2021-03-22 00:06:05 -0300

    adds overlay that closes settings once clicked

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,13 +6,23 @@ import {FaCog} from 'react-icons/fa';
 import { MainContext } from '../../context/app.context';
 
 const Navbar = () => {
+
   const [appContext, setAppContext] = useContext(MainContext)
+
+  const handleClose = (e?: MouseEvent) => {
+    const element = e.target as HTMLElement
+    if (element?.classList?.value.includes('overlay')) {
+      document.removeEventListener('click', handleClose);
+      setAppContext({...appContext, showSettings: false});
+    }
+  }
+
   return (
     <div className="navbar--container row">
       <SearchComponent />
       <div className="navbar--container-settings-btn"
-        onClick={() => setAppContext({...appContext, showSettings: !appContext.showSettings})}
-      ><FaCog style={{color: !appContext.showSettings ? '#99cbeb' : '#bf1140'}}/></div>
+        onClick={ () => {document.addEventListener('click', e => handleClose(e) ), setAppContext({...appContext, showSettings: !appContext.showSettings})}}
+      ><FaCog id="cog-icon" style={{color: !appContext.showSettings ? '#99cbeb' : '#bf1140'}}/></div>
     </div>
   );
 };

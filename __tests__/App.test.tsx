505aedc836740f06bec40a6ecbeebc528231d7de
Joaquim <johnboxcodes@gmail.com>
2021-03-23 18:22:36 -0300

    first passing test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import App from '../src/App'
 
-test('renders a message', async () => {
-  const { container, getByTitle } = await render(<App />)
-  expect(getByTitle('Address book')).toBeInTheDocument()
+test('App title must be set', async () => {
+  render(<App />)
+  const expectTitle = 'Address book'
+  await waitFor(() => expect(document.title).toEqual(expectTitle));
 })

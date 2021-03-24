7c9b91201c907a007e2a8500bdf5956cbc01c9ca
Joaquim <johnboxcodes@gmail.com>
2021-03-23 21:56:45 -0300

    adds test to api fetching status

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { render, waitFor, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
 import App from '../src/App'
+import UsersList from '../src/components/users_list/Users_List'
 
 test('App title must be set', async () => {
   render(<App />)
@@ -22,14 +25,39 @@ test('App should render main components', () => {
   expect(settingsBtn).toBeInTheDocument()
 });
 
-test("Don't show settings if cog icon is not clicked", async() => {
+test("Should not show settings if cog icon is not clicked", async() => {
   const { getByText } = render(<App />)
     expect(getByText('Settings')).toBeInTheDocument();
 });
 
-test("When cog icon is clicked it must show settings", async() => {
+test("should show settings when cog icon is clicked it ", async() => {
   const { getByText } = render(<App />)
   const settingsBtn = screen.getByTestId('settings-btn')
   await fireEvent.click(settingsBtn)
   expect(getByText('Settings')).toBeInTheDocument();
 });
+
+
+// const server = setupServer(
+//   // capture "GET /greeting" requests
+//   rest.get('https://randomuser.me/api/?seed=sherpany&results=50', (req, res, ctx) => {
+//     // respond using a mocked JSON body
+//     //return res(ctx.json({ greeting: 'hello there' }))
+//     //status    data  error
+//     return res(ctx.json({ status: 'fetching', data: [], error: '' }))
+//   })
+// )
+
+
+test('Should show "loading..." when request status is "fetching" ', async () => {
+  const serverFetching = setupServer(
+    rest.get('/api/', (_, res, ctx) => {
+      return res(ctx.json({ status: 'fetching', data: [], error: '' }))
+    })
+  )
+  beforeAll(() => serverFetching.listen())
+  afterEach(() => serverFetching.resetHandlers())
+  afterAll(() => serverFetching.close())
+  render(<UsersList />)
+  expect(screen.getByRole('heading')).toHaveTextContent('loading...')
+})
